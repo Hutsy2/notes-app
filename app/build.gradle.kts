834fada153510cab3c71588867d64867129b19ef
sam muigai <muigai.samuel21@students.dkut.ac.ke>
2023-11-06 12:18:26 +0300

    Added lifecycle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ android {
 
 dependencies {
 
+    val lifecycle_version = "2.6.2"
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -63,4 +65,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
 }
